8cfdaca0a20ad07c282eb32b0ef7ef4df7b89a1f
VinceEmond <vince.emond@icloud.com>
2022-02-15 23:48:24 +0000

    Added description at top of file

--- survey.js
+++ survey.js
@@ -1,4 +1,7 @@
-// Survery.js
+// Survey.js
+// To help out our fellow users, let's create a quick survey app which asks the user a bunch of questions like their favourite music, activity, food, sport, etc. It will then generate a profile description for them to use online, like the example demonstrated above.
+
+
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
 const rl = readline.createInterface({ input, output });
